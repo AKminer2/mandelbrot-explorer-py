4170f390fcf478df542391841438dbaa39977ea6
Ari Kanbur <152089347+AKminer2@users.noreply.github.com>
2023-12-03 09:42:41 -0700

    add code comments to check_number.py

--- src/check_number.py
+++ src/check_number.py
@@ -14,15 +14,19 @@
     # You should have received a copy of the GNU General Public License
     # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# check_number function
 def check_number(n: complex, max_iter: int):
+    # iterates max_iter times. If z becomes > 2: not part of set else: is part of set
     z = 0
     for i in range(0, max_iter):
         z = (z*z) + n
         if abs(z.real) > 2 or abs(z.imag) > 2:
             # // print(i / max_iter)
             
+            # if not part of set: return color, darker the less iterations
             return int(255 * (i / max_iter))
     else:
+        # if is part of set: return white
         return 255
     
 # // check_number(-1, 1000)
